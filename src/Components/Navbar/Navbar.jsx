899564dee168dc4877bff9f212c5500ac768bd5f
azikenp <azikenp26@gmail.com>
2024-06-21 17:49:56 +0100

    new

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
         </li>
         <li
           onClick={() => {
-            setMenu("women");
+            setMenu("womens");
           }}
         >
           Women
