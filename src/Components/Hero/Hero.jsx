8a6b8885db13c214518fdc63303f8faa5fa57726
azikenp <azikenp26@gmail.com>
2024-06-21 20:07:49 +0100

    changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
         </div>
         <div className="hero-latest-button">
             <div>Latest Collection</div>
-            <img src={arrow_icon} alt="" />
+            <img src={arrow_icon} alt="arrow" />
         </div>
       </div>
       <div className="hero-right">
