986bbac2c0eccc6a6bfc817eb038e7a9e81a0b05
azikenp <azikenp26@gmail.com>
2024-06-21 19:49:29 +0100

    changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Hero.css";
 import hand_icon from '../Assets/hand_icon.png'
+import arrow_icon from '../Assets/arrow.png'
+import hero_image from '../Assets/hero_image.png'
 
 const Hero = () => {
   return (
@@ -13,9 +15,16 @@ const Hero = () => {
                 <img src={hand_icon} alt="hand_icon" />
             </div>
             <p>collections</p>
+            <p>for everyone</p>
+        </div>
+        <div className="hero-latest-button">
+            <div>Latest Collection</div>
+            <img src={arrow_icon} alt="" />
         </div>
       </div>
-      <div className="hero-right"></div>
+      <div className="hero-right">
+        <img src={hero_image} alt="hero image" />
+      </div>
     </div>
   );
 };
