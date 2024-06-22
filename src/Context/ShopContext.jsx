e0a83c7484606fe0ba6f51d1561cbebcb8bb558c
azikenp <azikenp26@gmail.com>
2024-06-22 18:23:39 +0100

    final

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -22,7 +22,24 @@ const ShopContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
 
-  const ContextValue = { all_product, cartItems, addToCart, removeFromCart };
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = all_product.find((product) => product.id === Number(item));
+        totalAmount += cartItems[item] * itemInfo.new_price;
+      }
+      return totalAmount;
+    }
+  };
+
+  const ContextValue = {
+    getTotalCartAmount,
+    all_product,
+    cartItems,
+    addToCart,
+    removeFromCart,
+  };
 
   return (
     <ShopContext.Provider value={ContextValue}>
